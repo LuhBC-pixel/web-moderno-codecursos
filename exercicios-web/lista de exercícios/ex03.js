2e60a7c358dfee3f7dfa91dafffedf12c2a102a7
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-11-07 17:44:53 -0300

    Cria a resposta do exercicio 3

---
+++ exercicios-web/lista de exercícios/ex03.js
@@ -0,0 +1,7 @@
+// Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+
+function expoente(a, b) {
+    return console.log(a**b);
+}
+
+expoente(2, 3);
