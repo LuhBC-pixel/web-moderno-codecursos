10d99b3ebd1d0f46fdbaf15d2de6cd2d71c93f6c
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-11-08 14:54:06 -0300

    Adiciona resolução do ex10

---
+++ exercicios-web/lista de exercícios/ex10.js
@@ -0,0 +1,9 @@
+// Crie uma função que verifica se um número inteiro passado como parêmetro é divisível por 3 e retorne true
+// ou false.
+
+const divisívelPor3 = numero => numero % 3 == 0 ? true : false;
+
+console.log(divisívelPor3(6));
+console.log(divisívelPor3(5));
+console.log(divisívelPor3(1));
+console.log(divisívelPor3(3));
