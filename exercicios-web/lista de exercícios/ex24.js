4cf19f8281239358cacfcd52e5c78a4df6f26923
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-11-09 11:21:18 -0300

    Adiciona resolução do ex 24

---
+++ exercicios-web/lista de exercícios/ex24.js
@@ -0,0 +1,12 @@
+// Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.
+
+function helloWorld() {
+    let repetir = 0;
+
+    while (repetir < 12) {
+        console.log('Hello World!');
+        repetir++;
+    }
+}
+
+helloWorld();
