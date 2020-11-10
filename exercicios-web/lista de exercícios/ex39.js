1dd0986c094db7fb60c4434e6da7022ed584c395
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-11-09 21:21:01 -0300

    Adiciona resolução de ex 39

---
+++ exercicios-web/lista de exercícios/ex39.js
@@ -0,0 +1,18 @@
+// Crie uma função que receba dois vetores de igual tamanho e troque seus elementos de modo que o
+// primeiro elemento do vetorA passe a ser o primeiro elemento do vetorB e vice versa e assim sucessivamente.
+// Faça a troca sem utilizar uma variável auxiliar.
+
+function trocaVetores(vetorA, vetorB) {
+    for (let i = 0; i < vetorA.length; i++) {
+        vetorA[i] = vetorA[i] + vetorB[i];
+        vetorB[i] = vetorA[i] - vetorB[i];
+        vetorA[i] = vetorA[i] - vetorB[i];
+    }
+    console.log('Vetor A: ', vetorA);
+    console.log('Vetor B: ', vetorB);
+}
+
+let vetorA = [1, 2, 3]
+let vetorB = [4, 5, 6]
+
+trocaVetores(vetorA, vetorB)
