b1f9ee4b00fb713bc2c829e7a7df5897e15728da
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-11-13 15:41:16 -0300

    Mudanças do diretório e adiciona solução do primeiro exercício

---
+++ exercicios-web/lista de exercícios 2/ex01.js
@@ -0,0 +1,8 @@
+// Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a função) e com ponto de exclamação "!" no final.   
+
+function cumprimentar(string) {
+    return console.log(`Olá, ${string}!`)
+}
+
+cumprimentar('Leonardo');
+cumprimentar('Maria');
