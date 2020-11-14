0791ae5e8a701f0341739450f4b65397bad52c77
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-11-14 17:15:44 -0300

    Cria solução do ex 21

---
+++ exercicios-web/lista de exercícios 2/ex21.js
@@ -0,0 +1,8 @@
+// Criar uma função que receba um array de números e retorne o menor número desse array.
+
+function menorNumero(array) {
+    return console.log(Math.min(...array));
+}
+
+menorNumero([10, 5, 35, 65]);
+menorNumero([5, -15, 50, 3]);
