dda1a3540d0b5d5e412dc9cd2ad8104e5adf7dfb
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-12-21 17:37:16 -0300

    Implementa alterações do componente Navegação

--- exercicios-web/react/crud/frontend/src/components/template/Footer.jsx
+++ exercicios-web/react/crud/frontend/src/components/template/Footer.jsx
@@ -3,5 +3,8 @@ import React from 'react';
 
 export default props =>
     <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> Luisa<span className="text-danger"> B</span>C</strong>
+        </span>
     </footer>
